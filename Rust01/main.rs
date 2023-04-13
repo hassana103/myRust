6443d58d4e0ac41899f2cda610a5ffe174f47459
hassana103 <hassan.a103@gmail.com>
2023-04-13 09:47:57 -0700

    hello world project

---
+++ Rust01/main.rs
@@ -0,0 +1,12 @@
+//chapter 1
+//this is my first rust project
+//i have created a directory and cd to it
+//started the vs code 
+//created a file called main.rs (this file)
+
+// there should always be a main function - main function is the starting point of your program
+// just like Java but much simpler ;) 
+// if you dont have a main function like this you will get an error
+fn main(){
+    println!("hello rust");
+}
